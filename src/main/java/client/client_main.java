eb488507a0f1df0dcf3953d96e3d4ab39c361f11
kongcheng123 <1175642794@qq.com>
2018-04-08 23:05:15 +0800

    first push

---
+++ src/main/java/client/client_main.java
@@ -0,0 +1,8 @@
+package client;
+
+/**
+ * @author xy
+ * @date 2018/04/08 23:05
+ */
+public class client_main {
+}
---
+++ src/main/java/server/server_main.java
@@ -0,0 +1,8 @@
+package server;
+
+/**
+ * @author xy
+ * @date 2018/04/08 23:04
+ */
+public class server_main {
+}
